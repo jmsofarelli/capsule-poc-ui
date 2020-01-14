99afeaaef0ef3ad5e62ce11ce3ef98b5da72a5a7
Jefferson Sofarelli <jmsofarelli@gmail.com>
2020-01-14 13:01:05 +0000

    Added redux, react-redux and redux thunk. Enhanced directories structure. Configured redux.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import App from './components/app/App'
+import reducers from './reducers'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+        <App />
+    </Provider>,
+    document.querySelector('#root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
